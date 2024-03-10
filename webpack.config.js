e2bd774425e704a46de7061bcaf9e831bffd8ea6
Angelo <avagenas14@gmail.com>
2024-03-10 11:55:20 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const path = require("path");
+const deps = require("./package.json").dependencies;
+module.exports = (_, argv) => ({
+  entry: "./src/index",
+  mode: "development",
+  output: {
+    publicPath:
+      argv.mode === "development"
+        ? "http://localhost:8092/"
+        : "https://bellmedia.ca/awegrid", // Production
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 8092,
+    historyApiFallback: true,
+  },
+  output: {
+    publicPath: "auto",
+    chunkFilename: "[id].[contenthash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "grid",
+      filename: "remoteEntry.js",
+      remotes: {
+        host: "host@http://localhost:8080/remoteEntry.js",
+      },
+      exposes: {
+        "./Grid": "./src/components/Grid",
+      },
+      shared: {
+        ...deps,
+        react: {
+          requiredVersion: deps.react,
+          singleton: true,
+          eager: true,
+        },
+        "react-dom": {
+          requiredVersion: deps["react-dom"],
+          singleton: true,
+          eager: true,
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+});
